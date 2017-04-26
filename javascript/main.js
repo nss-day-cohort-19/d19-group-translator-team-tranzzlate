2734d502df12fd7ecc7d360f9d0cc2317ba5362a
Dylan Smith <jeffdylansmith@gmail.com>
2017-04-26 09:01:06 -0500

    one more commit

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+console.log("hey");
+var lang = document.getElementById("lang");
+var input = document.getElementById("input");
+var button = document.getElementById("translate");
+
+var inputArray = input.value.split(" ");
+
+
+
+button.addEventListener("click", whichOne ){
+	if (lang.value == 0) {
+
+	} else if (lang.value == 1){
+
+	} else if (lang.value == 2){
+
+	} else if (lang.value == 3){
+
+	} else {
+		alert("please select a value");
+	}
+}
+
+var Tranzlate  
